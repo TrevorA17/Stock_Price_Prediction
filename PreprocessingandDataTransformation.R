c2834b504bc0df55b298ea558a2f0e255c9cea6e
Trevor Augustine <122263629+TrevorA17@users.noreply.github.com>
2024-04-23 10:59:08 +0300

    load dataset for preprocessing

---
+++ PreprocessingandDataTransformation.R
@@ -0,0 +1,19 @@
+# Load dataset
+stock_data <- read.csv("data/stock.csv", colClasses = c(
+  Date = "Date",
+  High = "numeric",
+  Low = "numeric",
+  Open = "numeric",
+  Close = "numeric",
+  Volume = "numeric",
+  Adj_Close = "numeric"
+))
+
+# Display the structure of the dataset
+str(stock_data)
+
+# View the first few rows of the dataset
+head(stock_data)
+
+# Open the dataset in a viewer window
+View(stock_data)
